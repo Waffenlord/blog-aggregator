2ae1abffc4377039e199b9f977b3db8cb8e9fe7f
William Sánchez <swilliamd.sanchez@gmail.com>
2023-12-07 19:36:26 -0500

    Establish database connection

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/Waffenlord/blog-aggregator/internal/database"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database: ", err)
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
 
 
 	router := chi.NewRouter()
@@ -28,6 +51,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", readinessEndpoint)
 	v1Router.Get("/err", errorEndpoint)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
