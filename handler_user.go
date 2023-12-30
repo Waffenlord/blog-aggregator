f948e1e8194e0a049fd103cec248ccfdd3ecbad9
William Sánchez <swilliamd.sanchez@gmail.com>
2023-12-29 20:50:44 -0500

    Retrieve posts for user

--- handler_user.go
+++ handler_user.go
@@ -41,3 +41,17 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
 	responseWithJSON(w, 200, databaseUserToUser(user))
 }
+
+func (apiCfg *apiConfig) handlerGetPostsForUser(w http.ResponseWriter, r *http.Request, user database.User) {
+	posts, err := apiCfg.DB.GetPostsForUser(r.Context(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit: 10,
+	})
+
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Couldn't get posts: %v", err))
+		return
+	}
+	responseWithJSON(w, 200, databasePostsToPosts(posts))
+
+}
--- models.go
+++ models.go
@@ -16,23 +16,6 @@ type User struct {
 }
 
 
-type Feed struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Name      string    `json:"name"`
-	Url       string	`json:"url"`
-	UserID    uuid.UUID	`json:"user_id"`
-}
-
-type FeedFollow struct {
-	ID        uuid.UUID `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	UserID    uuid.UUID	`json:"user_id"`
-	FeedID    uuid.UUID	`json:"feed_id"`
-}
-
 func databaseUserToUser(dbUser database.User) User {
 	return User{
 		ID: dbUser.ID,
@@ -41,6 +24,16 @@ func databaseUserToUser(dbUser database.User) User {
 		Name: dbUser.Name,
 		APIKey: dbUser.ApiKey,
 	}
+}
+
+
+type Feed struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Name      string    `json:"name"`
+	Url       string	`json:"url"`
+	UserID    uuid.UUID	`json:"user_id"`
 }
 
 func databaseFeedToFeed(dbFeed database.Feed) Feed {
@@ -63,6 +56,13 @@ func databaseFeedsToFeeds(dbFeeds []database.Feed) []Feed {
 }
 
 
+type FeedFollow struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	UserID    uuid.UUID	`json:"user_id"`
+	FeedID    uuid.UUID	`json:"feed_id"`
+}
 
 func databaseFeedFollowToFeedFollow(dbFeedFollow database.FeedFollow) FeedFollow {
 	return FeedFollow{
@@ -81,3 +81,42 @@ func databaseFeedFollowsToFeedFollows(dbFeedFollows []database.FeedFollow) []Fee
 	}
 	return feedFollows
 }
+
+type Post struct {
+	ID          uuid.UUID	`json:"id"`
+	CreatedAt   time.Time	`json:"created_at"`
+	UpdatedAt   time.Time	`json:"updated_at"`
+	Title       string		`json:"title"`
+	Description *string		`json:"description"`
+	PublishedAt time.Time	`json:"published_at"`
+	Url         string		`json:"url"`
+	FeedID      uuid.UUID	`json:"feed_id"`
+}
+
+
+func databasePostToPost(dbPost database.Post) Post {
+	var description *string
+	if dbPost.Description.Valid {
+		description = &dbPost.Description.String
+	}
+	return Post{
+		ID: dbPost.ID,
+		CreatedAt: dbPost.CreatedAt,
+		UpdatedAt: dbPost.UpdatedAt,
+		Title: dbPost.Title,
+		Description: description,
+		PublishedAt: dbPost.PublishedAt,
+		Url: dbPost.Url,
+		FeedID: dbPost.FeedID,
+	}
+}
+
+func databasePostsToPosts(dbPosts []database.Post) []Post {
+	posts := []Post{}
+	for _, dbPost := range dbPosts {
+		posts = append(posts, databasePostToPost(dbPost))
+	}
+
+	return posts
+}
+
